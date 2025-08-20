7f02d02cae540b0823a77552af9439cfaa4cdb33
Navya P <navyap304@gmail.com>
2025-08-20 18:59:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let btn1 = document.querySelector("#btn1");
+
+// btn1.onclick = () =>{
+//     console.log("btn1 was clicked")
+// };
+
+// btn1.onclick = (e) =>{
+//     console.log(e)
+// };
+
+// let div = document.querySelector("div");
+// div.onmouseover = () =>{
+//     console.log("Hovered")
+// };
+
+btn1.addEventListener("click", () => {
+    console.log("button1 was clicked")
+})
